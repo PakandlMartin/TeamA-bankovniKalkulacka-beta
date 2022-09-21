4611615c8ab877ce0195fe3b3af0600060fde26c
Cesar Lopes de Mendonca <cesar.beloca@tutanota.com>
2022-09-21 10:33:35 +0200

    component userForm created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule
