355c5c7bc9da57f209dee7196ebb8016894ac252
Martin Pakandl <nzdm.martin@seznam.cz>
2022-09-21 15:25:09 +0200

    Validace údajů před http requestem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormCalculationComponent } from './form-calculation/form-calculation.component';
 import { FormDetailsComponent } from './form-details/form-details.component';
 import { FormContactComponent } from './form-contact/form-contact.component';
+import { FormCalculationComponent } from './form-calculation/form-calculation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormCalculationComponent,
     FormDetailsComponent,
-    FormContactComponent
-    FormsModule,
-    ReactiveFormsModule
+    FormContactComponent,
+    FormCalculationComponent,
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule
   ],
   providers: [],
--- src/app/form-calculation/form-calculation.component.ts
+++ src/app/form-calculation/form-calculation.component.ts
@@ -1,38 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormControl, Validators, FormsModule} from '@angular/forms';
-
-
 @Component({
   selector: 'app-form-calculation',
   templateUrl: './form-calculation.component.html',
   styleUrls: ['./form-calculation.component.css']
 })
 export class FormCalculationComponent implements OnInit {
-infoAboutCalculation: FormGroup;
-btnDisabled = true;
+amountInput: number = 0;
+numOfMOnthsInput: number = 0;
+btnActive: boolean = false;
 
-constructor() { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.initForm()
+
+
   }
 
-private initForm() {
-  let amount = '';
-  let dueDate = '';
+  calculate(amountCalculate, numOfMonthsCalculate ) {
+  }
+  
+  changeOfAmount(amountChange) {
+    this.amountInput = amountChange.target.value
+    this.changeBtnActive()
+  }
 
-  this.infoAboutCalculation = new FormGroup({
-    'amount' : new FormControl(amount, Validators.required),
-    'dueDate' : new FormControl(dueDate, Validators.required)
-  });
-};
+  changeOfNumOfMonths(numChange) {
+    this.numOfMOnthsInput = numChange.target.value
+    this.changeBtnActive()
+  }
 
-onSubmit() {
-  console.log(this.infoAboutCalculation)
-}
+  changeBtnActive() {
+    if (this.amountInput && this.numOfMOnthsInput) {
+      this.btnActive = true;
+      } else {
+        this.btnActive = false;
+      }
+  }
 
-
-
+  // changeOfNumOfMonths()
 
 }
