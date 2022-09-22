d9d8b77048cfa7f67f198fcb5f45c56ab479eea3
petra-landerova <petra.landerova@plus4u.net>
2022-09-22 13:44:30 +0200

    commit thursday

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormDetailsComponent } from './form-details/form-details.component';
@@ -15,24 +11,37 @@ import { FormCalculationComponent } from './form-calculation/form-calculation.co
 import { HttpRequestsService } from './http-requests.service';
 import {DetailsComponent} from "./details/details.component";
 import {UserFormComponent} from "./user-form/user-form.component";
+import {RouterModule, Routes} from "@angular/router";
+import { AuthComponent } from './auth/auth.component';
+import { AuthEmployeeComponent } from './auth/auth-employee/auth-employee.component';
+
+const appRoutes: Routes = [
+  { path: '', component: FormCalculationComponent },
+  { path: 'calculator', component: FormCalculationComponent },
+  { path: 'client', component: UserFormComponent },
+  { path: 'login', component: AuthComponent },
+  { path: 'employee', component: AuthEmployeeComponent },
+  ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
     HeaderComponent,
     FormDetailsComponent,
     FormContactComponent,
     FormCalculationComponent,
     DetailsComponent,
     UserFormComponent,
+    AuthComponent,
+    AuthEmployeeComponent,
   ],
   imports: [
     FormsModule,
     ReactiveFormsModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
 
   ],
   providers: [HttpRequestsService],
